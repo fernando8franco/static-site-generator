5c8a6396f7f879ae4af3c721f93787fd1e4891bf
fernando8franco <francoduranluisfernando@gmail.com>
2024-09-10 15:14:27 -0600

    htmlnode test values & test repr added

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -14,5 +14,19 @@ class TestHTMLNode(unittest.TestCase):
         props = html_node.props_to_html()
         self.assertEqual(props, ' href="https://www.google.com" target="_blank"')
 
+    def test_values(self):
+        html_node = HTMLNode("div", "Hello")
+        self.assertEqual(html_node.tag, "div")
+        self.assertEqual(html_node.value, "Hello")
+        self.assertEqual(html_node.children, None)
+        self.assertEqual(html_node.props, None)
+
+    def test_repr(self):
+        html_node = HTMLNode("a", "google", None, {"href":"https://www.google.com"})
+        self.assertEqual(
+            html_node.__repr__(),
+            "HTMLNode(a, google, children: None, {'href': 'https://www.google.com'})"
+        )
+
 if __name__ == "__main__":
     unittest.main()
