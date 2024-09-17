93bf302d6ba4d68ebba371fe302cb6c63a5ea5cc
fernando8franco <francoduranluisfernando@gmail.com>
2024-09-17 16:19:53 -0600

    split nodes image & link methods & tests added

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,7 +7,7 @@ text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
 
-from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
+from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link
 from textnode import TextNode
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -49,3 +49,35 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertListEqual(matches,
                             [("to youtube", "https://www.youtube.com/@bootdotdev")]
                             )
+        
+    def test_split_nodes_image(self):
+        node = TextNode(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            text_type_text,
+        )
+        new_nodes  = split_nodes_image([node])
+        self.assertListEqual(new_nodes,
+                            [
+                                TextNode("This is text with a ", text_type_text),
+                                TextNode("rick roll", text_type_image, "https://i.imgur.com/aKaOqIh.gif"),
+                                TextNode(" and ", text_type_text),
+                                TextNode(
+                                    "obi wan", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"
+                                ),
+                            ])
+        
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            text_type_text,
+        )
+        new_nodes  = split_nodes_link([node])
+        self.assertListEqual(new_nodes,
+                            [
+                                TextNode("This is text with a link ", text_type_text),
+                                TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+                                TextNode(" and ", text_type_text),
+                                TextNode(
+                                    "to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"
+                                ),
+                            ])
