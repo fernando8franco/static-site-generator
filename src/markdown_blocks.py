22d89249640f123cb9f36794821e93ea03e6d073
fernando8franco <francoduranluisfernando@gmail.com>
2024-09-25 15:06:22 -0600

    block types method & tests added

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,9 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -10,3 +16,39 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block)
 
     return filtered_blocks
+
+def block_to_block_type(block):
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+        
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    
+    lines = block.split("\n")
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph    
+        return block_type_quote
+    
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph    
+        return block_type_unordered_list
+    
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph    
+        return block_type_unordered_list
+    
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}."):
+                return block_type_paragraph
+            i += 1
+        return block_type_ordered_list
+    
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import block_to_block_type, markdown_to_blocks
 
 class TestMarkdownBlocks(unittest.TestCase):
 
@@ -30,3 +30,30 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                              ['# This is a heading', 
                               'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
                               '* This is another list item'])
+        
+    def test_block_to_block_type(self):
+        block_type = block_to_block_type("paragraph")
+        self.assertEqual(block_type, "paragraph")
+
+        block_type = block_to_block_type("# heading")
+        self.assertEqual(block_type, "heading")
+        block_type = block_to_block_type("### heading")
+        self.assertEqual(block_type, "heading")
+        block_type = block_to_block_type("###### heading")
+        self.assertEqual(block_type, "heading")
+
+        block_type = block_to_block_type("```code\ncode```")
+        self.assertEqual(block_type, "code")
+
+        block_type = block_to_block_type(">quote\n>quote")
+        self.assertEqual(block_type, "quote")
+
+        block_type = block_to_block_type("* unordered_list\n* unordered_list")
+        self.assertEqual(block_type, "unordered_list")
+        block_type = block_to_block_type("- unordered_list\n- unordered_list")
+        self.assertEqual(block_type, "unordered_list")
+
+        block_type = block_to_block_type("* unordered_list\n* unordered_list")
+        self.assertEqual(block_type, "unordered_list")
+        block_type = block_to_block_type("1. ordered_list\n2. ordered_list\n3. ordered_list")
+        self.assertEqual(block_type, "ordered_list")
